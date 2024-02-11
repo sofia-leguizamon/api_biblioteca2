a2ef65d7cb3691b30c45bce7d281c631f11f042f
franco balles <sofiavioleta.leguizamon@gmail.com>
2024-02-11 13:58:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const { auth } = require("express-oauth2-jwt-bearer");
+// Importamos el Middleware Error Handler
+const errorHandler = require('./middlewares/errorHandler');
+
+// Configuracion Middleware con el Servidor de Autorización
+const autenticacion = auth({
+    audience: "http://localhost:3000/api/productos",
+    issuerBaseURL: "https://dev-utn-frc-iaew.auth0.com/",
+    tokenSigningAlg: "RS256",
+    });
+
+const app = express(); 
+app.use(express.json());
+
+// Importamos el Router de Libros
+const librosRouter = require('./routes/libros');
+
+//configuramos el middleware de autenticacion 
+app.use('/libros', autenticacion, librosRouter);
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+console.log('Servidor iniciado en el puerto 3000');
+});
